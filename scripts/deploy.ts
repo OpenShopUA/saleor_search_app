b6b6f394bf67a294e2f3711daf0150f9dd7366af
Yurii Kosyi <y.kosiy@gmail.com>
2025-02-11 15:47:11 +0200

    fix some files

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,9 +1,9 @@
-import { exportSentryReleaseEnvironmentVariable } from "@saleor/sentry-utils";
+// import { exportSentryReleaseEnvironmentVariable } from "@saleor/sentry-utils";
 import { execSync } from "node:child_process";
 
 import packageJson from "../package.json";
 
-exportSentryReleaseEnvironmentVariable(packageJson.version);
+// exportSentryReleaseEnvironmentVariable(packageJson.version);
 
 execSync("pnpm run build", { stdio: "inherit" });
 execSync("pnpm run migrate", { stdio: "inherit" });
